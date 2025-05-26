f794ceece999cd9a9e9c034f4608ef059526ecf1
Dhyey Vora <54502473+DhyeyVora-1706@users.noreply.github.com>
2025-05-27 00:57:26 +0530

    Added Auth and restrcited without having login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUserInfo } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation(); // When route is changed and if token is not there it will redirect to login
+
+  useEffect(() => {
+    fetchUser();
+  }, [location.pathname]); // It is a dependent parameter with path if if path changes API will be called to check for auth
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUserInfo(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <Navbar />
